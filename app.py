e45fb06cbef486b4327f305ed61590ca490d1612
Andrew Beaton <andrewbeaton123@gmail.com>
2025-05-26 14:40:17 +0100

    ✨feature: Working web interface to play against a post fast api hosted model

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import logging
+import requests
+import json
 from tic_tac_toe_game import TicTacToe  # Import your TicTacToe class
 
 app = Flask(__name__)
@@ -17,13 +19,37 @@ def make_move():
     # For simplicity, we'll use a global game instance
     global game
     if not game.is_game_over() and (row, col) in game.get_valid_moves():
+        
+            
+            
+        game.make_move(row, col)
+        winner = game.winner
+        board_state = game.board.tolist()
+        #
         try:
-            #if game.current_player == 2:
 
-            game.make_move(row, col)
-            winner = game.winner
-            board_state = game.board.tolist()
-            return jsonify({'board': board_state, 'winner': winner})
+            url = "http://127.0.0.1:8000/next_move"
+            headers = {"Content-Type": "application/json"}
+            payload = {
+            "current_player": 2,
+            "game_state": board_state
+                }
+            try:
+                response = requests.post(url, headers=headers, data=json.dumps(payload))
+                response.raise_for_status()  # Raise an exception for bad status codes
+                next_move = response.json()["move"]
+                logging.info(f"Next Move:{next_move}")
+                game.make_move(*game.get_valid_moves()[int(next_move)])  
+                final_board = game.board.tolist()
+                winner = game.winner
+                return jsonify({'board': final_board, 'winner': winner})
+            
+            except requests.exceptions.RequestException as e:
+                print(f"Error sending request: {e}")
+            
+            except json.JSONDecodeError:
+                print("Error decoding JSON response.")
+            
         except ValueError as e:
             logging.error(f"Invalid move: {e}")
             return jsonify({'error': str(e)}), 400
